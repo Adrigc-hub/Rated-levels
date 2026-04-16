66350ed9cf31c4e2f47ac296e0b97e4b4cc80faa
Adrigc-hub <adrigc1214@gmail.com>
2026-04-16 17:33:09 -0600

    Actualizar todo

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,38 @@
-class $modify(MenuLayer) {
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
+        // Run the original init() first
         if (!MenuLayer::init()) return false;
 
-        // 1. Obtener tamaño de ventana
         auto winSize = CCDirector::sharedDirector()->getWinSize();
 
-        // 2. Crear el sprite del botón
+        // 1. Create the button sprite
+        // Using a standard sprite frame name from the game's sheets
         auto sprite = CCSprite::createWithSpriteFrameName("GJ_playBtn_001.png");
         sprite->setScale(0.5f);
 
-        // 3. Crear el botón con una función 
+        // 2. Create the button
+        // menu_selector is fine, but Geode also supports modern lambda callbacks!
         auto btn = CCMenuItemSpriteExtra::create(
             sprite, 
             this, 
-            menu_selector(MenuLayer::onMoreGames)
+            menu_selector(MenuLayer::onMoreGames) // Reusing an existing function
         );
 
-        // 4. Crear menú y posicionar
+        // 3. Create the menu container
+        // It's better to add the button to the existing 'main-menu' 
+        // or create a new CCMenu to handle layout/touch priority.
         auto menu = CCMenu::create();
         menu->setPosition({ winSize.width / 2, (winSize.height / 2) + 80.f });
+        menu->setID("my-custom-menu"_spr); // Good practice for compatibility with other mods
 
         menu->addChild(btn);
         this->addChild(menu);
 
         return true;
-} 
-;}
+    }
+};
